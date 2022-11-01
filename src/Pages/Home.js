a391c7f6d208a7ed884d90cddf51f06e51cdcf05
aniqabd <aniq.hilman98@gmail.com>
2022-11-01 14:40:23 +0800

    Home page development

---
+++ src/Pages/Home.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "../App.css";
+import { Container } from "react-bootstrap";
+import { Row } from "react-bootstrap";
+import { Col } from "react-bootstrap";
+import { Image } from "react-bootstrap";
+import { Card } from "react-bootstrap";
+
+function Home() {
+  return (
+    <div class="App">
+      <Card>
+        <Card.Img variant="top" style={{height: "400px", width: "100%"}} src="/images/nus-logo.jpeg" />
+        <Card.Body>
+          <Card.Text style={{ fontSize: "30px", fontFamily: "-moz-initial" }}>
+            NUS is a leading research university in Asia
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </div>
+  );
+}
+
+export default Home;
